75388fb814f2dbbea46dfb492d5931c38fed1b85
Michelle <35238012+MissWibbon@users.noreply.github.com>
2019-11-11 14:07:39 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {BrowserRouter, Route, NavLink, Redirect, Switch} from 'react-router-dom'
+import NavBar from './navbar'
+import Landing from './landing'
+import Home from './home'
+import SignUp from './profile'
+import './styles/main.scss';
+function App() {
+  return (
+    <BrowserRouter>
+    <NavBar></NavBar>
+      <Switch>
+        <Route to exact path = '/login' component ={Landing}/>
+        <Route to exact path = '/home' component ={Home}/>
+        <Route to exact path = '/signup' component={SignUp}/>
+        <Route to exact path = '/' render= {()=><Redirect to ='/login'/>}/>
+      </Switch>
+    </BrowserRouter>
+  );
+}
+export default App;
